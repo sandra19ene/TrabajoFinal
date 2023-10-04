b88bc32812bb42c3cafb8e0a8f11539471bc2120
sandra19ene <chanaa75@gmail.com>
2023-10-04 20:13:03 -0300

    menu completo

--- src/vistas/menu.java
+++ src/vistas/menu.java
@@ -468,7 +468,7 @@ private DefaultTableModel modelo = new DefaultTableModel();
            modelo.setRowCount(0);
            cargarTabla();
         
-        
+        //salame
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
