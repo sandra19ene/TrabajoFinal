d20efe5183efd11230680c2254f09e79d36ede30
PatricioPaz3138 <patricio_paz_16@hotmail.com>
2023-10-04 19:51:26 -0300

    Agrego metodo para eliminar compra en CompraData

--- src/accesoADatos/CompraData.java
+++ src/accesoADatos/CompraData.java
@@ -1,26 +1,23 @@
-
 package accesoADatos;
 
 import entidades.Compra;
-import entidades.Proveedor;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalDate;
 import javax.swing.JOptionPane;
 
-
 public class CompraData {
+
     private Connection con = null;
 
     public CompraData() {
         con = Conexion.getConexion();
     }
-    
-    public void realizarCompra(int idProveedor, Compra comp){
+
+    public void realizarCompra(int idProveedor, Compra comp) {
         String sql = "INSERT INTO compra (idProveedor, fecha) VALUES (?,?)";
         try {
             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
@@ -28,7 +25,7 @@ public class CompraData {
             ps.setInt(1, idProveedor);
             ps.setDate(2, Date.valueOf(comp.getFecha()));
             ps.executeUpdate();
-            
+
             ResultSet rs = ps.getGeneratedKeys();
 
             if (rs.next()) {
@@ -37,9 +34,30 @@ public class CompraData {
 
             }
             ps.close();
-                      
+
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null,"Error al acceder a la tabla 'compra'/"+ ex.getMessage());
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla 'compra'/" + ex.getMessage());
         }
+    }
+
+    public void eliminarCompra(int idCompra) {
+        String sql = "DELETE FROM compra WHERE idCompra = ?";
+
+        PreparedStatement ps;
+        try {
+            ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, idCompra);
+            int resultado = ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (resultado == 1) {
+                JOptionPane.showMessageDialog(null, "Compra eliminada exitosamente");
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla compra " + ex.getMessage());
+
+        }
+    
     }
 }
