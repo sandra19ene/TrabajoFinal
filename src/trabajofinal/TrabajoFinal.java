9fa4f7b6f33a8832b3dccfe763033d593832f3bc
Martín Lescano <martin.m.lescano@gmail.com>
2023-10-06 18:30:53 -0300

    Entrega 1 CON VISTA carga de producto

--- src/trabajofinal/TrabajoFinal.java
+++ src/trabajofinal/TrabajoFinal.java
@@ -9,9 +9,9 @@ import entidades.Proveedor;
 public class TrabajoFinal {
 
     public static void main(String[] args) {
-//        ProductoData proData = new ProductoData();    
-//        Producto prod = new Producto(4,"Samsung Smart TV 50", "TV Smart 50 pulgadas Android SO", 99000.55, 400, true);
-//      //  proData.guardarProducto(prod);
+        ProductoData proData = new ProductoData();    
+        Producto prod = new Producto("Smart TV 32", "TV Smart 32", 99000.55, 400, true);
+        proData.guardarProducto(prod);
 //        proData.actualizarProducto(prod);
 //        proData.eliminarProducto(prod);
 
